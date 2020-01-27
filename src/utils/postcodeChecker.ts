8e8529dc5cb93e85aad234b9fe4171a13db5716b
Jamie Marsay <jamie@salt.agency>
2020-01-27 20:42:52 +0000

    have tidied up info windows slightly, made half postcode search valid

--- src/utils/postcodeChecker.ts
+++ src/utils/postcodeChecker.ts
@@ -1,4 +1,12 @@
 export const postcodeChecker = (p: string) => {
-  const postcodeRegEx = /[A-Z]{1,2}[0-9]{1,2} ?[0-9][A-Z]{2}/i;
-  return postcodeRegEx.test(p) && p.length <= 8;
+  const shortCodeRegEx = /[A-Z]{1,2}[0-9]{1,2}/i;
+  const longPostCodeRegEx = /[A-Z]{1,2}[0-9]{1,2} ?[0-9][A-Z]{2}/i;
+
+  if (shortCodeRegEx.test(p) && p.length <= 4) {
+    return true;
+  } else if (longPostCodeRegEx.test(p) && p.length <= 8) {
+    return true;
+  } else {
+    return false;
+  }
 };
